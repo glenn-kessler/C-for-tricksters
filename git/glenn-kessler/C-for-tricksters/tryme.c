443569dd8f4d3abc0c6ba77f4992d45c07e70ca6
Glenn Kessler <info@glenn-kessler.de>
2018-05-24 05:36:49 -0700

    init tryme.c

---
+++ git/glenn-kessler/C-for-tricksters/tryme.c
@@ -0,0 +1,8 @@
+#include "stdio.h"
+
+printf("https://stackoverflow.com/questions/7780809/is-it-possible-to-print-out-only-a-certain-section-of-a-c-string-without-making");
+
+char yourString = "Whatever";
+
+// yes it is
+printf("print the first two letters of your String here: %*.*s", 2, yourString);
