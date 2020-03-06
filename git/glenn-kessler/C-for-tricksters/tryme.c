975e121610d2268d47c4ae4dfe6a25bc08ccafc8
Glenn Keßler <7099536+glenn-kessler@users.noreply.github.com>
2020-03-06 14:08:19 +0100

    added example code for usage of bit fields in C

--- git/glenn-kessler/C-for-tricksters/tryme.c
+++ git/glenn-kessler/C-for-tricksters/tryme.c
@@ -18,3 +18,43 @@ int main(void)
 	return 0;
 }
 
+
+/**********************************************
+******  Bit-field usage                 *******
+**********************************************/
+#include <stdio.h>
+#include <stdbool.h>
+
+typedef union
+{
+    struct                     ///< bit-field of 32 bit lenght (no name given)
+    {
+        int file         : 1;  ///< lowest bit
+        int dir          : 1;  ///< 2nd bit
+        int rwx          : 3;  ///< some example crc
+        int reserved     : 27; ///< the remaining 28 bits
+    };
+}BITFIELD_EXAMPLE;             ///< union to prevent memory 
+
+
+typedef struct
+{
+    int u32myVar;
+    BITFIELD_EXAMPLE bf;  ///< use bitfield
+
+} ST_EXAMPLE;
+
+static ST_EXAMPLE _container;  ///< global var
+
+int main(void)
+{
+
+
+    _container.u32myVar = 0xBADU;
+
+    _container.bf.file = true;
+    _container.bf.dir  = false;
+    _container.bf.rwx  = 1+2+4;
+
+    printf("%x", _container.bf);
+}
