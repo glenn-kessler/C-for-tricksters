719e196e6582dba31741e074bddc48e401ba68aa
Glenn Keßler <7099536+glenn-kessler@users.noreply.github.com>
2024-06-17 12:03:42 +0200

    Update useful.h
    add bit wise reverse u32

--- git/glenn-kessler/useful.h
+++ git/glenn-kessler/useful.h
@@ -1,5 +1,9 @@
 #ifndef USEFUL_H
 #define USEFUL_H
 
+#define BYTE_WISE_REVERSE_U32(n) ((uint32_t) (((n & 0x000000FF) << 24) | \
+                                              ((n & 0x0000FF00) <<  8) | \
+                                              ((n & 0x00FF0000) >>  8) | \
+                                              ((n & 0xFF000000) >> 24)))
 
 #endif // USEFUL_H
